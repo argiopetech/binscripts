9cb8581bc83678f91d5d668494fbc4e5a9eecb71
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-01-16 11:42:42 -0500

    Add new goal categories
    - Just dates aren't enough for a goals-tracking application
    - Add generic units, percentages, money, and separate the difference between things that begin on a date and things that end on a date.
    - Affects only Goals.hs

--- Goals.hs
+++ Goals.hs
@@ -3,7 +3,17 @@ module Main where
 import Data.Time
 import Text.Printf
 
-data Goals = Countdown (Integer, Int, Int) String
+-- I waffle on the appropriate separator for metrics from message, so it's configurable here.
+-- Space after the separator is built-in, space before is not.
+separator = ":"
+-- separator = " until"
+
+data Goals = Beginning (Integer, Int, Int) String
+           | End (Integer, Int, Int) String
+           | Money Integer Integer String
+           | Generic Integer Integer String String
+           | Percent Double String
+           | Section
   deriving ( Read )
 
 main = do
@@ -21,12 +31,41 @@ main = do
 
 tupleToDay (year, month, day) = fromGregorian year month day
 
-showGoal today (Countdown date event) =
+-- Beginnings and endings must be treated differently
+--
+-- Endings run till the end of the last day, so there's an extra day (counting today)
+--
+-- Beginnings run till the end of the day preceding (because the countdown ends once the day has
+-- begun).  We accomplish this by not counting "today".
+showGoal today (Beginning date event) =
   let d     = tupleToDay date
+      delta = diffDays d today
 
-      -- I usually want to know "days till the end of the day"
-      -- The number of days till the end of today is ~1 day, so we add 1
-      -- Otherwise, the number of days till the end of today is 0
+  in formatDays delta event
+
+showGoal today (End date event) =
+  let d     = tupleToDay date
       delta = 1 + diffDays d today
 
-  in printf "%3d" delta ++ " days till " ++ event
+  in formatDays delta event
+
+showGoal _ (Money current goal event) =
+  printf "%9s%s %s" (formatMoney $ goal - current) separator event
+
+showGoal _ (Generic current goal units event) =
+  printf "%4d %4s%s %s" (goal - current) units separator event
+
+showGoal _ (Percent current event) =
+  printf "%4.0f %%%s    %s" (100 * (1.0 - current)) separator event
+
+showGoal _ Section =
+  printf "%4s" ""
+
+formatDays delta event =
+  printf "%4d days%s %s" delta separator event
+
+formatMoney m =
+  let m' = reverse $ show m
+  in '$':(reverse $ go m')
+  where go a | length a < 3 = a
+             | otherwise    = take 3 a ++ ',':(go $ drop 3 a)
